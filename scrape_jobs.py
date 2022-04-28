f5ba2c6b186f1c65d2e83c0bdff180a7c50f1d65
Vignesh Sankar Iyer <vignesh.sankar.iyer.1999@gmail.com>
2022-04-28 12:48:46 +0800

    Update scrape_jobs.py

--- scrape_jobs.py
+++ scrape_jobs.py
@@ -1,1 +1,20 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+WSC_URL = "https://wsc-sports.com/careers/"
+
+WSC_page = requests.get(WSC_URL)
+
+careers_soup = BeautifulSoup(WSC_page.content,"html.parser")
+
+url_results = careers_soup.find(id="d").find_all('a', href=True)
+
+url_container = []
+
+for a in url_results:
+    url_container.append(a['href'])
+
+for url in url_container:
+    page = requests.get(url)
+    job_soup = BeautifulSoup(page.content,"html.parser")
+
